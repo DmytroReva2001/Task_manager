3c61a76fed3ac38d79134e1444c230f05702643b
Dmytro Reva <dmytroreva2001@gmail.com>
2024-09-05 08:20:19 +0200

    Tasks salen ordenados por la fecha

--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,9 @@ from .models import Task
 from .serializers import TaskSerializer
 
 class TaskListCreate(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
+    queryset = Task.objects.order_by('date')
     serializer_class = TaskSerializer
 
 class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
+    queryset = Task.objects.order_by('date')
     serializer_class = TaskSerializer
