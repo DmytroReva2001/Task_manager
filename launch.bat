e6793956474c0c0a135ac670ec7d21fa94ceabd5
DmytroReva2001 <dmytro.reva@atos.net>
2025-06-10 16:37:01 +0200

    Update launch.bat
    Mejorado el script de inicialización:
    
    1) Más rápido
    2) Más seguro
    3) Autoupdate
    4) Reutilización de entorno

--- launch.bat
+++ launch.bat
@@ -1,11 +1,27 @@
 @echo off
 
-rem Iniciar servidor Django en segundo plano en la misma consola
+rem Crear un entorno virtual si no existe
+if not exist "venv\" (
+    echo Creando entorno virtual...
+    python -m venv venv
+)
+
+rem Activar el entorno virtual
+echo Activando entorno virtual...
+call venv\Scripts\activate
+
+rem Actualizar requirements.txt
+echo Actualizando dependencias...
+pip install -r requirements.txt --upgrade
+
+rem Iniciar servidor Django en segundo plano
 echo Iniciando servidor Django...
 start /b "" python manage.py runserver
 
-rem Iniciar servidor React en segundo plano en la misma consola
+rem Iniciar servidor React en segundo plano
 echo Iniciando servidor React...
 cd frontend
 start /b "" npm start
 cd ..
+
+echo Todos los servidores están en funcionamiento.
